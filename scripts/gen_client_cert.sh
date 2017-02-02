f64459bc48d888ced773570a7cce070d570ee856
Bjoern Stierand <bjoern-github@innovention.de>
2017-02-02 07:00:55 +0100

    generete CCD entry for host

--- scripts/gen_client_cert.sh
+++ scripts/gen_client_cert.sh
@@ -6,4 +6,10 @@ read CLIENTNAME
 docker-compose exec openvpn easyrsa build-client-full $CLIENTNAME nopass
 docker-compose exec openvpn ovpn_getclient $CLIENTNAME > certs/$CLIENTNAME.ovpn
 
+echo -n "Enter the last part of your desired client IP [10.42.0.xx]: "
+read IP_OCTET
+
+IP_OCTET_1=$(expr ${IP_OCTET} + 1)
+echo "ifconfig-push 10.42.0.${IP_OCTET} 10.42.0.${IP_OCTET_1}" > /opt/openvpn/conf/ccd/${CLIENTNAME}
+
 echo "Client certificate is now in certs/$CLIENTNAME.ovpn"
