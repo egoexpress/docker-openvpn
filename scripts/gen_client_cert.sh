2e3ba65a20c253f1e55349426d5cc9f215f2a8ae
Björn Stierand <bjoern-github@innovention.de>
2018-10-21 12:29:46 +0200

    specify cipher suite

--- scripts/gen_client_cert.sh
+++ scripts/gen_client_cert.sh
@@ -12,4 +12,6 @@ read IP_OCTET
 IP_OCTET_1=$(expr ${IP_OCTET} + 1)
 echo "ifconfig-push 10.42.0.${IP_OCTET} 10.42.0.${IP_OCTET_1}" > /opt/openvpn/conf/ccd/${CLIENTNAME}
 
+echo "cipher AES-256-CBC" >> certs/$CLIENTNAME.ovpn
+
 echo "Client certificate is now in certs/$CLIENTNAME.ovpn"
